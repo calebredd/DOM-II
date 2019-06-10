6b3ec6cc7d32bf13d6841d1479a1deee7f18e108
calebredd <caleb.redd@yahoo.com>
2019-06-09 19:58:21 -0700

    Added new p element inside of intro section to layout where and which event listeners are used on the webpage along with stopPropagation and preventDefault methods.

--- js/index.js
+++ js/index.js
@@ -15,26 +15,29 @@ document.addEventListener("scroll", e => {
     document
       .querySelectorAll(".content-section")[0]
       .querySelector("img").style.height =
-      `${Math.round(e.timeStamp/100)}` + "px";
+      `${Math.round(e.timeStamp / 100)}` + "px";
   }
 });
-document.addEventListener("dblclick", e => {
-  if (e.timeStamp/100< 376) {
-    document
-      .querySelectorAll(".content-section")[1]
-      .querySelector("img").style.width =
-      `${Math.round(e.timeStamp/100)}` + "px";
-    document
-      .querySelectorAll(".content-section")[1]
-      .querySelector("img").style.height =
-      `${Math.round(e.timeStamp/100)}` + "px";
-  }
-});
+document
+  .querySelectorAll(".content-section")[1]
+  .querySelector("img")
+  .addEventListener("dblclick", e => {
+    if (e.timeStamp / 100 < 376) {
+      document
+        .querySelectorAll(".content-section")[1]
+        .querySelector("img").style.width =
+        `${Math.round(e.timeStamp / 100)}` + "px";
+      document
+        .querySelectorAll(".content-section")[1]
+        .querySelector("img").style.height =
+        `${Math.round(e.timeStamp / 100)}` + "px";
+    }
+  });
 
 body.addEventListener("mousemove", e => {
-  body.style.background = `rgb(${Math.round(e.pageX / 3)},${Math.round(
-    e.pageY / 3
-  )},${Math.round(e.pageY / 3)})`;
+  body.style.background = `rgb(${Math.round(e.screenX / 3)},${Math.round(
+    e.screenY / 3
+  )},${Math.round(e.screenY / 2)})`;
 });
 
 textBox.style.display = "none";
@@ -98,17 +101,57 @@ busPic.onmouseleave = function(e) {
   return;
 };
 busPic.onmousemove = function(e) {
+  e.stopPropagation();
+  body.style.background = `rgb(${Math.round(e.pageX / 4)},${Math.round(
+    e.pageY-70
+  )},${Math.round(e.pageY-70)})`;
   if (
-    e.clientX > 559 &&
-    e.clientX < 778 &&
-    (e.clientY > 177 && e.clientY < 269)
+    e.pageX > 559 &&
+    e.pageX < 778 &&
+    (e.pageY > 177 && e.pageY < 269)
   ) {
     e.target.style.border = "10px solid #ffd800";
   } else {
     e.target.style.border = "10px solid white";
   }
 };
-
+window.onload = e => {
+  var p = document.createElement("p");
+  var newP = document.querySelector(".intro").appendChild(p);
+  newP.innerHTML = `I'm a new paragraph! I was created when the page was loaded. Let me introduce you to my buddies, the Events:
+  <!==// Types of event listeners used:
+  // Scroll
+  // Double Click
+  // Mousemove
+    // mousemove
+  // onClick
+    // click/preventDefault();
+  // KeyDown
+  // Select
+  // onblur
+  // mouseenter
+  // mouseleave
+  //load==>
+  <br><br>Types of event listeners used on this page:<br>
+  <br>1.) Load-The fact that you can read this is proof that my event listener works!
+  <br>2.) MouseMove-The background color is determined by the X and Y coordinates of the mouse
+  <br>--TOP-LEFT:Black; BOTTOM-RIGHT:WHITE; BOTTOM-LEFT:BLUE; TOP-RIGHT:Red;--
+  <br>3.) Click-Click my h1 title and you can change it to whatever you want!
+  <br>--Just don't leave it blank or you will break the page!--
+  <br>4.) KeyDown-If you press enter after changing the title is will display your new title.
+  <br>5.) Blur-Or you can click away instead of pressing enter.
+  <br>6.) Select-If you click on the title again, the entire title will be preselected/highlighted.
+  <br>7.) MouseEnter-When the mouse enters the school bus picture, the border changes for the picture.
+  <br>---It's worth noting, the border is special when hovering over the school bus part of the picture too!
+  <br>8.) MouseLeave-When the mouse leaves the school bus picture, the border changes again.
+  <br>9.) Scroll-The first picture after the school bus changes size based on how long ago the page was loaded and the last scroll.
+  <br>10.) DoubleClick-The next picture also varies in size based on how long ago the page was loaded, but updates only when double clicked. 
+  <br><br> stopPropogation()
+  <br> -Used inside of the busPic to have a varying background color while the mouse moves inside of the pic.  
+  <br><br> preventDefault()
+  <br> -This is used to prevent the nav links from reloading the page.  
+`;
+};
 //     <img src="img/fun-bus.jpg" alt="bus in the sand">
 //     <h2>Welcome To Fun Bus!</h2>
 //     <p>Traveling expedition modern, clean webdesign blogger clean website theme website modern. Design pretty design, travelblogger adventure WordPress wanderlust theme blogger website expedition theme travelblogger. Adventure fun traveler pretty design website expedition.</p>
